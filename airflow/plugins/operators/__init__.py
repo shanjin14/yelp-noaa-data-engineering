2485c9e35980d2b08936d131d82d50ac8991ef16
George Seah <shanjin14@users.noreply.github.com>
2021-08-05 14:28:58 +0800

    Update __init__.py

--- airflow/plugins/operators/__init__.py
+++ airflow/plugins/operators/__init__.py
@@ -1,13 +1,16 @@
 from operators.stage_redshift import StageToRedshiftOperator
-from operators.load_fact import LoadFactOperator
-from operators.load_dimension import LoadDimensionOperator
 from operators.data_quality import DataQualityOperator
 from operators.s3_to_redshift import S3ToRedshiftOperator
+from operators.s3_upload_object import S3UploadObjectOperator
+from operators.load_data_redshift import LoadDataOperator
+from operators.s3_delete_objects import S3DeleteObjectsOperator
 
 __all__ = [
+    'S3ToRedshiftOperator',
     'StageToRedshiftOperator',
-    'LoadFactOperator',
-    'LoadDimensionOperator',
     'DataQualityOperator',
-    'S3ToRedshiftOperator'
+    'S3UploadObjectOperator',
+    'S3DeleteObjectsOperator',
+    'LoadDataOperator'
 ]
+
